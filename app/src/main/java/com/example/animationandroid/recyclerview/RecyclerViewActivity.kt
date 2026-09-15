cae010454fce26c8f37c9f3aeac9fbac3d3d2ff9
mansukh makwana <mansukh.m@celcius.in>
2026-09-15 14:45:57 +0530

    15-09-2026 RecyclerViewActivity

--- app/src/main/java/com/example/animationandroid/recyclerview/RecyclerViewActivity.kt
+++ app/src/main/java/com/example/animationandroid/recyclerview/RecyclerViewActivity.kt
@@ -3,162 +3,141 @@ package com.example.animationandroid.recyclerview
 import android.app.Dialog
 import android.graphics.Color
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.WindowManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.core.graphics.drawable.toDrawable
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.animationandroid.R
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.example.animationandroid.databinding.ActivityRecyclerViewBinding
+import com.example.animationandroid.databinding.DialogAddUpdateContactBinding
 
 class RecyclerViewActivity : AppCompatActivity() {
 
-    private var toolbar: Toolbar? = null
-    private var recyclerView: RecyclerView? = null
-    private var floatingActionButton: FloatingActionButton? = null
-    var arrayContact = ArrayList<ContactModel>()
+    private lateinit var binding: ActivityRecyclerViewBinding
+    private var recyclerViewContactAdapter: RecyclerViewContactAdapter? = null
+
+    private var arrayContact = ArrayList<ContactModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_recycler_view)
+
+        binding = ActivityRecyclerViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        toolbar = findViewById(R.id.toolbar)
-        recyclerView = findViewById(R.id.recyclerView)
-        floatingActionButton = findViewById(R.id.fbAddContact)
+        setupToolbar()
+        setupRecyclerView()
+        addContacts()
+        setupAdapter()
+        setupAddContactButton()
 
-        //androidx.appcompat.widget.Toolbar
-        setSupportActionBar(toolbar)
-        //android.widget
-        //setActionBar(toolbar)
+    }
 
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.title = "Title"
-        //toolbar?.setTitle("Title")
-        toolbar?.setSubtitle("Sub Title")
+    private fun setupAddContactButton() {
+        binding.fbAddContact.setOnClickListener {
 
-        recyclerView?.layoutManager = LinearLayoutManager(this)
-
-        arrayContact.add(ContactModel(R.drawable.ic_contact_profile, "Steven Smith", "98004654564"))
-        arrayContact.add(ContactModel(R.drawable.ic_contact_profile, "Steven Smith", "98004654564"))
-        arrayContact.add(ContactModel(R.drawable.ic_contact_profile, "Steven Smith", "98004654564"))
-        arrayContact.add(ContactModel(R.drawable.ic_contact_profile, "Steven Smith", "98004654564"))
-        arrayContact.add(ContactModel(R.drawable.ic_contact_profile, "Steven Smith", "98004654564"))
-        arrayContact.add(ContactModel(R.drawable.ic_contact_profile, "Steven Smith", "98004654564"))
-        arrayContact.add(ContactModel(R.drawable.ic_contact_profile, "Steven Smith", "98004654564"))
-        arrayContact.add(ContactModel(R.drawable.ic_contact_profile, "Steven Smith", "98004654564"))
-        arrayContact.add(ContactModel(R.drawable.ic_contact_profile, "Steven Smith", "98004654564"))
-        arrayContact.add(ContactModel(R.drawable.ic_contact_profile, "Steven Smith", "98004654564"))
-        arrayContact.add(ContactModel(R.drawable.ic_contact_profile, "Steven Smith", "98004654564"))
-        arrayContact.add(ContactModel(R.drawable.ic_contact_profile, "Steven Smith", "98004654564"))
-        arrayContact.add(ContactModel(R.drawable.ic_contact_profile, "Steven Smith", "98004654564"))
-        arrayContact.add(ContactModel(R.drawable.ic_contact_profile, "Steven Smith", "98004654564"))
-        arrayContact.add(ContactModel(R.drawable.ic_contact_profile, "Steven Smith", "98004654564"))
-        arrayContact.add(ContactModel(R.drawable.ic_contact_profile, "Steven Smith", "98004654564"))
-        arrayContact.add(ContactModel(R.drawable.ic_contact_profile, "Steven Smith", "98004654564"))
-        arrayContact.add(ContactModel(R.drawable.ic_contact_profile, "Steven Smith", "98004654564"))
-        arrayContact.add(ContactModel(R.drawable.ic_contact_profile, "Steven Smith", "98004654564"))
-        arrayContact.add(ContactModel(R.drawable.ic_contact_profile, "Steven Smith", "98004654564"))
-        arrayContact.add(ContactModel(R.drawable.ic_contact_profile, "Steven Smith", "98004654564"))
-        arrayContact.add(ContactModel(R.drawable.ic_contact_profile, "Steven Smith", "98004654564"))
-        arrayContact.add(ContactModel(R.drawable.ic_contact_profile, "Steven Smith", "98004654564"))
-        arrayContact.add(ContactModel(R.drawable.ic_contact_profile, "Steven Smith", "98004654564"))
-        arrayContact.add(ContactModel(R.drawable.ic_contact_profile, "Steven Smith", "98004654564"))
-        arrayContact.add(ContactModel(R.drawable.ic_contact_profile, "Steven Smith", "98004654564"))
-        arrayContact.add(ContactModel(R.drawable.ic_contact_profile, "Steven Smith", "98004654564"))
-        arrayContact.add(ContactModel(R.drawable.ic_contact_profile, "Steven Smith", "98004654564"))
-        arrayContact.add(ContactModel(R.drawable.ic_contact_profile, "Steven Smith", "98004654564"))
-        arrayContact.add(ContactModel(R.drawable.ic_contact_profile, "Steven Smith", "98004654564"))
-        arrayContact.add(ContactModel(R.drawable.ic_contact_profile, "Steven Smith", "98004654564"))
-        arrayContact.add(ContactModel(R.drawable.ic_contact_profile, "Steven Smith", "98004654564"))
-
-        val recyclerViewContactAdapter = RecyclerViewContactAdapter(this, arrayContact)
-        recyclerView?.setAdapter(recyclerViewContactAdapter)
-
-        floatingActionButton?.setOnClickListener {
             val dialog = Dialog(this@RecyclerViewActivity)
-            dialog.setContentView(R.layout.dialog_add_update_contact)
+            val dialogBinding = DialogAddUpdateContactBinding.inflate(LayoutInflater.from(this))
+            dialog.setContentView(dialogBinding.root)
+
             dialog.window?.setLayout(
                 WindowManager.LayoutParams.MATCH_PARENT,
                 WindowManager.LayoutParams.WRAP_CONTENT
             )
             dialog.window?.setBackgroundDrawable(Color.TRANSPARENT.toDrawable())
 
-            val editTextName = dialog.findViewById<EditText>(R.id.edtName)
-            val editTextNumber = dialog.findViewById<EditText>(R.id.edtNumber)
-            val btnAdd = dialog.findViewById<Button>(R.id.btnAdd)
-            val textViewContact = dialog.findViewById<TextView>(R.id.tvAddContact)
+            dialogBinding.apply {
+                tvAddContact.text = "Add Contact"
+                btnAdd.text = "Add"
 
-            btnAdd.setOnClickListener {
-                val name = editTextName.text.toString().trim()
-                val number = editTextNumber.text.toString().trim()
+                btnAdd.setOnClickListener {
+                    val name = dialogBinding.edtName.text.toString().trim()
+                    val number = dialogBinding.edtNumber.text.toString().trim()
 
-                if (name.isEmpty()) {
-                    Toast.makeText(
-                        this@RecyclerViewActivity,
-                        "Please Enter Contact Name!",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    return@setOnClickListener
-                }
-                if (number.isEmpty()) {
-                    Toast.makeText(
-                        this@RecyclerViewActivity,
-                        "Please Enter Mobile Numer",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    return@setOnClickListener
-                }
+                    if (name.isEmpty()) {
+                        Toast.makeText(this@RecyclerViewActivity, "Please Enter Contact Name!", Toast.LENGTH_SHORT).show()
+                        return@setOnClickListener
+                    }
+                    if (number.isEmpty()) {
+                        Toast.makeText(this@RecyclerViewActivity, "Please Enter Mobile Numer", Toast.LENGTH_SHORT).show()
+                        return@setOnClickListener
+                    }
 
-                arrayContact.add(
-                    ContactModel(
-                        img = R.drawable.ic_contact_profile,
-                        name = name,
-                        number = number
+                    arrayContact.add(
+                        ContactModel(
+                            img = R.drawable.ic_contact_profile,
+                            name = name,
+                            number = number
+                        )
                     )
-                )
-                recyclerViewContactAdapter.notifyItemInserted(arrayContact.size - 1)
-                recyclerView!!.scrollToPosition(arrayContact.size - 1)
+                    val newPosition = arrayContact.lastIndex
 
-                Toast.makeText(
-                    this@RecyclerViewActivity,
-                    "Add Contact Successfully",
-                    Toast.LENGTH_SHORT
-                ).show()
+                    recyclerViewContactAdapter?.notifyItemInserted(newPosition)
+                    binding.recyclerView.scrollToPosition(newPosition)
 
-                dialog.dismiss()
+                    Toast.makeText(this@RecyclerViewActivity, "Add Contact Successfully", Toast.LENGTH_SHORT).show()
+
+                    dialog.dismiss()
+                }
             }
+
             dialog.setCancelable(false)
             dialog.show()
         }
     }
 
+    private fun setupAdapter() {
+        recyclerViewContactAdapter = RecyclerViewContactAdapter(this, arrayContact)
+        binding.recyclerView.setAdapter(recyclerViewContactAdapter)
+    }
+
+    private fun addContacts() {
+        repeat(32) {
+            arrayContact.add(
+                ContactModel(
+                    img = R.drawable.ic_contact_profile,
+                    name = "Steven Smith",
+                    number = "98004654564"
+                )
+            )
+        }
+    }
+
+    private fun setupRecyclerView() {
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            title = "Title"
+            subtitle = "Sub Title"
+        }
+    }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         MenuInflater(this).inflate(R.menu.option_menu, menu)
-        return super.onCreateOptionsMenu(menu)
+        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
-        val itemId = item.itemId
-
-        when (itemId) {
+        when (item.itemId) {
             R.id.option_new -> {
                 Toast.makeText(this, "Create New File", Toast.LENGTH_SHORT).show()
             }
--- app/src/main/java/com/example/animationandroid/recyclerview/RecyclerViewContactAdapter.kt
+++ app/src/main/java/com/example/animationandroid/recyclerview/RecyclerViewContactAdapter.kt
@@ -9,124 +9,131 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
 import android.view.animation.AnimationUtils
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.TextView
 import android.widget.Toast
 import androidx.core.graphics.drawable.toDrawable
 import androidx.recyclerview.widget.RecyclerView
 import com.example.animationandroid.R
+import com.example.animationandroid.databinding.DialogAddUpdateContactBinding
+import com.example.animationandroid.databinding.RowContactBinding
 
 class RecyclerViewContactAdapter(
-    var context: Context,
-    var arrContacts: ArrayList<ContactModel>
+    private val context: Context,
+    private val arrContacts: ArrayList<ContactModel>
 ) : RecyclerView.Adapter<RecyclerViewContactAdapter.ViewHolder>() {
 
     private var lastPosition = -1
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(context).inflate(R.layout.row_contact, parent, false)
-        return ViewHolder(view)
+
+        val binding = RowContactBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
         val contact = arrContacts[position]
 
-        holder.imgContact.setImageResource(contact.img)
-        holder.txtName.text = contact.name
-        holder.txtNumber.text = contact.number
-
-        setAnimation(holder.itemView, position)
-
-        holder.editContact.setOnClickListener {
-
-            val dialog = Dialog(context)
-            dialog.setContentView(R.layout.dialog_add_update_contact)
-
-            dialog.window?.setLayout(
-                WindowManager.LayoutParams.MATCH_PARENT,
-                WindowManager.LayoutParams.WRAP_CONTENT
-            )
-            dialog.window?.setBackgroundDrawable(Color.TRANSPARENT.toDrawable())
-
-            val editTextName = dialog.findViewById<EditText>(R.id.edtName)
-            val editTextNumber = dialog.findViewById<EditText>(R.id.edtNumber)
-            val btnAdd = dialog.findViewById<Button>(R.id.btnAdd)
-            val textViewContact = dialog.findViewById<TextView>(R.id.tvAddContact)
-
-            textViewContact.text = "Update Contact"
-            btnAdd.text = "Update"
-
-            editTextName.setText(contact.name)
-            editTextNumber.setText(contact.number)
-
-            btnAdd.setOnClickListener {
-
-                val name = editTextName.text.toString().trim()
-                val number = editTextNumber.text.toString().trim()
-
-                if (name.isEmpty()) {
-                    Toast.makeText(
-                        context,
-                        "Please Enter Contact Name!",
-                        Toast.LENGTH_SHORT
-                    ).show()
+        holder.binding.apply {
+            imgProfile.setImageResource(contact.img)
+            tvName.text = contact.name
+            tvContactNumber.text = contact.number
+            imgEdit.setOnClickListener {
+                val currentPosition = holder.bindingAdapterPosition
+                if (currentPosition == RecyclerView.NO_POSITION) {
                     return@setOnClickListener
+                } else {
+                    showUpdateDialog(currentPosition)
                 }
-                if (number.isEmpty()) {
-                    Toast.makeText(
-                        context,
-                        "Please Enter Mobile Numer",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    return@setOnClickListener
-                }
-                arrContacts[position] = ContactModel(arrContacts[position].img, name, number)
-                notifyItemChanged(position)
-
-                Toast.makeText(context, "Update Contact Successfully", Toast.LENGTH_SHORT).show()
-
-                dialog.dismiss()
             }
-            dialog.setCancelable(false)
-            dialog.show()
+
+            imgDelete.setOnClickListener {
+                val currentPosition = holder.bindingAdapterPosition
+                if (currentPosition == RecyclerView.NO_POSITION) {
+                    return@setOnClickListener
+                }
+                showDeleteDialog(currentPosition)
+            }
         }
 
-        holder.deleteContact.setOnClickListener {
-            val builder = AlertDialog.Builder(context)
-                .setTitle("Delete Contact")
-                .setMessage("Are Your Sure Want To Delete Contact?")
-                .setIcon(R.drawable.ic_baseline_delete_forever_24)
-                .setPositiveButton(
-                    "YES"
-                ) { dialogInterface, i: Int ->
-                    arrContacts.removeAt(position)
-                    notifyItemRemoved(position)
-                    Toast.makeText(context, "Delete Contact Successfully", Toast.LENGTH_SHORT).show()
-                    dialogInterface.dismiss()
-                }
-                .setNegativeButton(
-                    "NO"
-                ) { dialogInterface, i: Int ->
-                    dialogInterface!!.dismiss()
-                }
-            builder.show()
+        setAnimation(holder.binding.root, position)
+
+    }
+
+    private fun showDeleteDialog(position: Int) {
+        val builder = AlertDialog.Builder(context)
+            .setTitle("Delete Contact")
+            .setMessage("Are Your Sure Want To Delete Contact?")
+            .setIcon(R.drawable.ic_baseline_delete_forever_24)
+            .setPositiveButton(
+                "YES"
+            ) { dialogInterface, i: Int ->
+                arrContacts.removeAt(position)
+                notifyItemRemoved(position)
+                Toast.makeText(context, "Delete Contact Successfully", Toast.LENGTH_SHORT).show()
+                dialogInterface.dismiss()
+            }
+            .setNegativeButton(
+                "NO"
+            ) { dialogInterface, i: Int ->
+                dialogInterface!!.dismiss()
+            }
+        builder.show()
+    }
+
+    private fun showUpdateDialog(position: Int) {
+        val dialog = Dialog(context)
+        val binding = DialogAddUpdateContactBinding.inflate(LayoutInflater.from(context))
+        dialog.setContentView(binding.root)
+
+        dialog.window?.setLayout(
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.WRAP_CONTENT
+        )
+        dialog.window?.setBackgroundDrawable(Color.TRANSPARENT.toDrawable())
+
+        val contact = arrContacts[position]
+
+        binding.tvAddContact.text = "Update Contact"
+        binding.btnAdd.text = "Update"
+
+        binding.edtName.setText(contact.name)
+        binding.edtNumber.setText(contact.number)
+
+        binding.btnAdd.setOnClickListener {
+
+            val name = binding.edtName.text.toString().trim()
+            val number = binding.edtNumber.text.toString().trim()
+
+            if (name.isEmpty()) {
+                Toast.makeText(context, "Please Enter Contact Name!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (number.isEmpty()) {
+                Toast.makeText(context, "Please Enter Mobile Numer", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            arrContacts[position] = ContactModel(
+                img = contact.img,
+                name = name,
+                number = number
+            )
+            notifyItemChanged(position)
+
+            Toast.makeText(context, "Update Contact Successfully", Toast.LENGTH_SHORT).show()
+
+            dialog.dismiss()
         }
+        dialog.setCancelable(false)
+        dialog.show()
     }
 
     override fun getItemCount(): Int {
         return arrContacts.size
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val txtName: TextView = itemView.findViewById(R.id.tvName)
-        val txtNumber: TextView = itemView.findViewById(R.id.tvContactNumber)
-        val imgContact: ImageView = itemView.findViewById(R.id.imgProfile)
-        val editContact: ImageView = itemView.findViewById(R.id.imgEdit)
-        val deleteContact: ImageView = itemView.findViewById(R.id.imgDelete)
-    }
+    class ViewHolder(
+        val binding: RowContactBinding
+    ) : RecyclerView.ViewHolder(binding.root)
 
     private fun setAnimation(
         view: View,
