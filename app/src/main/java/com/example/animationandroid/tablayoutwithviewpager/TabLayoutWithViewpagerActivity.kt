a13254e3d6d8db73624407dd98e3c37fe6d607e8
mansukh makwana <mansukh.m@celcius.in>
2026-09-15 14:52:24 +0530

    15-09-2026 TabLayoutWithViewpagerActivity

--- app/src/main/java/com/example/animationandroid/tablayoutwithviewpager/TabLayoutWithViewpagerActivity.kt
+++ app/src/main/java/com/example/animationandroid/tablayoutwithviewpager/TabLayoutWithViewpagerActivity.kt
@@ -5,31 +5,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.viewpager.widget.ViewPager
 import com.example.animationandroid.R
-import com.google.android.material.tabs.TabLayout
+import com.example.animationandroid.databinding.ActivityTabLayoutWithViewpagerBinding
 
 class TabLayoutWithViewpagerActivity : AppCompatActivity() {
 
-    private var tabLayout: TabLayout? = null
-    private var viewPager: ViewPager? = null
+    private var binding: ActivityTabLayoutWithViewpagerBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_tab_layout_with_viewpager)
+
+        binding = ActivityTabLayoutWithViewpagerBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        tabLayout = findViewById(R.id.tabLayout)
-        viewPager = findViewById(R.id.viewPager)
-
         val viewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
-        viewPager!!.setAdapter(viewPagerAdapter)
+        binding?.viewPager?.setAdapter(viewPagerAdapter)
 
-        tabLayout!!.setupWithViewPager(viewPager)
+        binding?.tabLayout?.setupWithViewPager(binding?.viewPager)
     }
 }
